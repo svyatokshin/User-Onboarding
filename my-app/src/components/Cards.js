f137b0748cef55d037c5769bb5702179bf2f0b26
svyatoslav-okshin <svyatokshin@gmail.com>
2020-01-30 23:17:21 -0500

    updated styling

--- my-app/src/components/Cards.js
+++ my-app/src/components/Cards.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const CardContainer = styled.div`
-    margin:2% 25%;
+    margin: 2% 25%;
     background: rgb(2,0,36);
     background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(31,9,121,1) 42%, rgba(0,212,255,1) 100%);
     border-radius: 10px;
@@ -11,17 +11,18 @@ const CardContainer = styled.div`
 const Cards = ({person}) => {
     return (
         <div className="card-list">
-            <CardContainer>
+            
             {person.map(card => (
+                <CardContainer>
                 <div className="card" key={card.id}>
                     <h1>Member Created</h1>
                     <h2>Name: {card.name}</h2>
                     <h3>Email: {card.email}</h3>
                     <h4>Password Length: {card.password.length} characters</h4>
                 </div>
-                
+                </CardContainer>
             ))}
-            </CardContainer>
+            
         </div>
     );
 };
